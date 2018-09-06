02d646cee5e050b38550d186cbb2b26511934415
kerry456 <1598814730@qq.com>
2018-09-06 14:24:25 +0800

    Signed-off-by: kerry456 <1598814730@qq.com>

---
+++ cookiespool/config.py
@@ -0,0 +1,44 @@
+# Redis数据库地址
+REDIS_HOST = '127.0.0.1'
+
+# Redis端口
+REDIS_PORT = 6379
+
+# Redis密码，如无填None
+REDIS_PASSWORD = None
+
+# 产生器使用的浏览器
+BROWSER_TYPE = 'Firefox'
+
+#是否启用浏览器界面 False为不启用
+
+BROWSER_GUI = False
+
+
+# 产生器类，如扩展其他站点，请在此配置
+GENERATOR_MAP = {
+    'weibo': 'WeiboCookiesGenerator'
+}
+
+# 测试类，如扩展其他站点，请在此配置
+TESTER_MAP = {
+    'weibo': 'WeiboValidTester'
+}
+
+TEST_URL_MAP = {
+    'weibo': 'https://m.weibo.cn/'
+}
+
+# 产生器和验证器循环周期
+CYCLE = 120
+
+# API地址和端口
+API_HOST = '127.0.0.1'
+API_PORT = 5000
+
+# 产生器开关，模拟登录添加Cookies
+GENERATOR_PROCESS = True
+# 验证器开关，循环检测数据库中Cookies是否可用，不可用删除
+VALID_PROCESS = True
+# API接口服务
+API_PROCESS = True
